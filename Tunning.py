abe29b6378bb2771bdc5d7900aefd73946e6967f
VivekSharma2607 <sharmavishu760@gmail.com>
2021-06-21 17:00:21 +0530

    Update Tunning.py

--- Tunning.py
+++ Tunning.py
@@ -1,8 +1,5 @@
-#Hyperparameter tunning using RandomizedSearchCV
-#Logistic Regression()
-#RandomForestClassifier()
-#Tunning with GridSearch
-# Diffrent hyperprameter for our LogisticREgrssion model As it was the best
+#Tunning using RandomizedSearchCV is in JupyterNotebook
+#Tunning using GridSearchCV on Logistic Regressor and RandomForestClassifier 
 log_reg_grid = {"C" : np.logspace(-4 , 4 , 30) ,
                "solver" : ["liblinear"]}
 gs_log_reg = GridSearchCV(LogisticRegression(),
@@ -32,3 +29,29 @@ def abc(y_test , y_preds):
 abc(y_test , y_preds)
 #Classification Report
 print(classification_report(y_test , y_preds))
+#Creating Classification Report using Cross Validation Report
+## Checkig the best parameter
+gs_log_reg.best_params_
+clf = LogisticRegression(C = 1.3738237958832638 , solver = "liblinear")  
+# Calculating the accuracy
+clf_acc = cross_val_score(clf , x , y , cv = 5 , scoring = "accuracy")
+clf_acc = np.mean(clf_acc)
+clf_acc
+## Calculating the precision
+clf_pre = cross_val_score(clf , x , y , cv = 5 , scoring="precision")
+clf_pre = np.mean(clf_pre)
+clf_pre
+clf_recall = cross_val_score(clf , x , y , cv = 5 , scoring="recall")
+clf_recall = np.mean(clf_recall)
+clf_recall
+clf_f1 = cross_val_score(clf , x , y , cv = 5 , scoring="f1")
+clf_f1 = np.mean(clf_f1)
+clf_f1
+# Visualize the data in form of graph
+cv_metrics = pd.DataFrame({"Accuracy" : clf_acc,
+                          "Precision" : clf_pre,
+                          "Recall" : clf_recall,
+                          "F1-score" : clf_f1},
+                         index = [0])
+cv_metrics.T.plot.bar(title = "Cross Validation CLassification Matrix" ,
+                      legend = False );
